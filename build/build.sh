c28b2476d3914793af2f085a1c45dd1660144644
Codezz-ops <codezz-ops@protonmail.com>
2024-02-29 06:48:16 -0500

    Inital commit

---
+++ build/build.sh
@@ -0,0 +1,10 @@
+mkdir obj
+mkdir -p isodir/boot/grub
+i686-elf-as boot/boot.s -o obj/boot.o
+i686-elf-gcc -c kernel/kernel.c -o obj/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker/linker.ld -o zincOS.bin -ffreestanding -O2 -nostdlib obj/boot.o obj/kernel.o 
+cp zincOS.bin isodir/boot/zincOS.bin
+cp boot/grub.cfg isodir/boot/grub/grub.cfg
+grub2-file --is-x86-multiboot zincOS.bin
+grub2-mkrescue -o zincOS.iso isodir
+qemu-system-i386 -cdrom zincOS.iso
